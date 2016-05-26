48ef01d9c37a0d00dc33217aa6ee843dd67ce4a5
JimLin <jimzuolin@gmail.com>
2016-05-26 22:05:13 +0100

    English version Test.py

--- Test.py
+++ Test.py
@@ -5,11 +5,13 @@ from random import randint
 
 from AsyncDB import AsyncDB
 
-T = 10000
-M = 7000
+T = 10000  # iter times
+M = 10000  # range of keys and values
 NAME = 'Test.db'
 
 
+# The idea of the test is randomized controlled trial
+# cmp is a normal Python dict. The DB should act exactly the same with cmp
 async def acid_t():
     if isfile(NAME):
         remove(NAME)
@@ -17,12 +19,14 @@ async def acid_t():
     cmp = {}
     db = AsyncDB(NAME)
 
+    # due to async, need a coro to check output and expected value
     async def check(key, expect):
         output = await db[key]
         assert output == expect
 
+    # use randint to exec series of random operation
     for i in range(T):
-        # 增改
+        # setitem
         if randint(0, 1):
             rand_key = randint(0, M)
             rand_value = randint(0, M)
@@ -30,7 +34,7 @@ async def acid_t():
             cmp[rand_key] = rand_value
             db[rand_key] = rand_value
 
-        # 删
+        # delitem
         if randint(0, 1):
             rand_key = randint(0, M)
             print('del', rand_key)
@@ -38,7 +42,7 @@ async def acid_t():
                 del cmp[rand_key]
             del db[rand_key]
 
-        # 读
+        # getitem
         if randint(0, 1):
             rand_key = randint(0, M)
             expect = cmp.get(rand_key)
@@ -46,7 +50,7 @@ async def acid_t():
             ensure_future(check(rand_key, expect))
             await sleep(0)
 
-    # 遍历
+    # iter
     cmp_items = list(cmp.items())
     for key, value in cmp_items:
         print('iter', key)
@@ -60,7 +64,7 @@ async def acid_t():
         assert cmp[key] == value
     assert len(items) == len(cmp_items)
 
-    # 遍历的参数
+    # test params of iter
     max_i = len(cmp_items) - 1
     i_from = randint(0, max_i - 1)
     i_to = randint(i_from + 1, max_i)
@@ -74,7 +78,7 @@ async def acid_t():
     print('iter params OK')
     await db.close()
 
-    # 数据入库
+    # re-open the DB, it should still act like cmp
     db = AsyncDB(NAME)
     for key, value in cmp_items:
         print('po_iter', key)
