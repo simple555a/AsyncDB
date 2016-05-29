f1c32390855dd1fb980a266f42ab8d6ed42cc279
JimLin <jimzuolin@gmail.com>
2016-05-29 01:24:20 +0100

    Python 最终版

--- AsyncDB/AsyncDB.py
+++ AsyncDB/AsyncDB.py
@@ -18,39 +18,28 @@ class AsyncDB:
     def __init__(self, filename: str):
         self.cache = Cache()
         self.engine = Engine(filename)
-        self.open = True
 
     def __getitem__(self, key):
         async def coro():
             return self.cache[key] if key in self.cache else await self.engine.get(key)
 
-        self.assert_open()
         return coro()
 
     def __setitem__(self, key, value):
-        self.assert_open()
         if key not in self.cache or self.cache[key] != value:
             self.cache[key] = value
             self.engine.set(key, value)
 
     def pop(self, key):
-        self.assert_open()
         if key in self.cache:
             del self.cache[key]
         return self.engine.pop(key)
 
     def items(self, item_from=None, item_to=None, max_len=0, reverse=False):
-        self.assert_open()
         return self.engine.items(item_from, item_to, max_len, reverse)
 
     async def close(self):
-        self.assert_open()
-        self.open = False
         if self.engine.task_que.que:
             await self.engine.lock.acquire()
             await self.engine.lock.acquire()
         self.engine.close()
-
-    def assert_open(self):
-        if not self.open:
-            raise Exception('Closed DB')
